24b6b37db2956489ce9a1353ee86c45e00bd9363
brunaletsleticia <140714125+brunaletsleticia@users.noreply.github.com>
2024-09-15 15:47:53 -0300

    Update HeapFilaPrioridade.java

--- src/main/java/HeapFilaPrioridade.java
+++ src/main/java/HeapFilaPrioridade.java
@@ -5,13 +5,14 @@ public class HeapFilaPrioridade implements FilaPrioridade {
 	public HeapFilaPrioridade(int capacidade) {
 		this.heap = new Heap(capacidade);
 	}
- 
+
 	// adicionar o pair no heap
-	public void add(String elemento, int prioridade) {}
+	public void add(String elemento, int prioridade) {
+		this.heap.add(elemento, prioridade);
+	}
 
 	// recuperar a raiz
 	public String removeNext() {
-		return "";
+		return this.heap.remove();
 	}
-
 }
